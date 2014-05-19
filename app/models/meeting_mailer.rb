0c903f96aa6daf3cfd7e53e76fe1bd61d7583244
Nathanael Burt <nathanael.burt@gmail.com>
2014-05-19 10:53:56 -0600

    users will be emailed once they have successfully scheduled a meeting

---
+++ app/models/meeting_mailer.rb
@@ -0,0 +1,9 @@
+class MeetingMailer < ActionMailer::Base
+
+  default from: "meetings@example.com"
+
+  def meeting_scheduled
+    mail(to: "employees@example.com", subject: "A meeting has been scheduled!")
+  end
+
+end
